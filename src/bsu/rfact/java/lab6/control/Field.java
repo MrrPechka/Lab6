b696965fdb83da0568014b02dac933b0ba83fe8a
Huk Anna <gukanna606@gmail.com>
2021-04-07 23:06:16 +0300

    method run and little methods in Field

--- src/bsu/rfact/java/lab6/control/Field.java
+++ src/bsu/rfact/java/lab6/control/Field.java
@@ -2,11 +2,15 @@ package bsu.rfact.java.lab6.control;
 
 import bsu.rfact.java.lab6.entity.BouncingBall;
 
+import javax.swing.plaf.basic.BasicTabbedPaneUI;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Timer;
+
+import static com.sun.java.accessibility.util.AWTEventMonitor.addMouseListener;
 
 public class Field {
     private boolean paused;
@@ -20,4 +24,42 @@ public class Field {
     private static double time;
     private BouncingBall initBall = null;
     private List<BouncingBall> balls = new ArrayList<>(10);
+    
+    public Field() {
+        setBackground(Color.WHITE);
+        addMouseListener(new BasicTabbedPaneUI.MouseHandler());
+        addMouseMotionListener(new MouseMotionHandler());
+        repaintTimer.start();
+        selectBall = false;
+    }
+
+    public void paintComponent(Graphics graphics) {
+        super.paintComponent(graphics);
+        Graphics2D canvas = (Graphics2D) graphics;
+        for (BouncingBall ball : balls) {
+            ball.paint(canvas);
+        }
+    }
+
+    private void setBackground(Color white) {
+    }
+
+    public void addBall() {
+        balls.add(new BouncingBall(this));
+    }
+
+    public synchronized void pause() {
+        paused = true;
+    }
+
+    public synchronized void resume() {
+        paused = false;
+        notifyAll();
+    }
+
+    public synchronized void canMove(BouncingBall bouncingBall) throws InterruptedException {
+        if (paused) {
+            wait();
+        }
+    }
 }
